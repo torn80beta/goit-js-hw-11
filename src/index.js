87c3d7723552693cf188e83229232df2494ae005
Alex <hotbox80@gmail.com>
2023-03-04 20:11:23 +0200

    gallery

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const refs = {
   formEl: document.querySelector('.search-form'),
   buttonEl: document.querySelector('.button'),
+  galleryEl: document.querySelector('.gallery'),
 };
 
 refs.formEl.addEventListener('submit', onFormSubmit);
@@ -17,25 +18,60 @@ refs.formEl.addEventListener('submit', onFormSubmit);
 
 //&safesearch=true
 
-function fetchUrl(targetUrl) {
-  return fetch(targetUrl)
+async function fetchUrl(targetUrl) {
+  let data;
+  await fetch(targetUrl)
     .then(response => response.json())
     .then(response => {
-      console.log(response);
-      return response;
+      //console.log(response);
+      data = [...response.hits];
+
+      return data;
     })
     .then(response => {
-      if (response.hits.length === 0) {
+      if (response.length === 0) {
         console.log(
+          response,
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
     });
+  //console.log(data);
+  return data;
+}
+
+function drawCards(data) {
+  const markup = data
+    .map(
+      ({ webformatURL, tags, likes, views, comments, downloads }) =>
+        `<div class="photo-card">
+                <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes: ${likes}</b>
+                </p>
+                <p class="info-item">
+                <b>Views: ${views}</b>
+                </p>
+                <p class="info-item">
+                <b>Comments :${comments}</b>
+                </p>
+                <p class="info-item">
+                <b>Downloads: ${downloads}</b>
+                </p>
+            </div>
+        </div>`
+    )
+    .join('');
+  console.log(markup);
+  refs.galleryEl.insertAdjacentHTML('beforeend', markup);
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
   const searchQuery = e.currentTarget.searchQuery.value;
   const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal`;
-  fetchUrl(url);
+  fetchUrl(url).then(data => {
+    drawCards(data);
+  });
 }
