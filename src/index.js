288c9da65dc6ca474e24d58bb5d41568b45cce7e
Alex <103459735+torn80beta@users.noreply.github.com>
2023-03-06 22:01:48 +0200

    scroll fix

--- src/index.js
+++ src/index.js
@@ -119,13 +119,17 @@ function drawCards(data) {
 }
 
 function scroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-  window.scrollBy({
-    top: cardHeight * 20,
-    behavior: 'smooth',
-  });
+  try {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 20,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // const searchParams = {
